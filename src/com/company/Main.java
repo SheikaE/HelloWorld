9f3e6b36bd200bb1b4cce51d0bfb3e32148cb993
Sheika Etienne <sheikaetienne@Sheikas-MacBook-Pro.local>
2020-08-26 16:52:27 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hi, who are you?");
+        String n = input.nextLine();
+
+        System.out.println("Hello, " + n + " nice to meet you. ");
+        System.out.println("How long have you been at Howard?");
+        int y = input.nextInt();
+        System.out.println("What is your GPA?");
+        float g = input.nextFloat();
+
+    }
+}
